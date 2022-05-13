3900492034977984210f986eea6a63ef37abeb71
ame <104745335+atinyame@users.noreply.github.com>
2022-05-13 18:56:22 -0500

    8kyu: Beginner Series #2 Clock

---
+++ 8kyu/past/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {}
+
+fn past(h: i32, m: i32, s: i32) -> i32 {
+    (h * 3600 + m * 60 + s) * 1000
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic_tests() {
+        assert_eq!(past(0, 1, 1), 61000);
+        assert_eq!(past(1, 1, 1), 3661000);
+        assert_eq!(past(0, 0, 0), 0);
+        assert_eq!(past(1, 0, 1), 3601000);
+        assert_eq!(past(1, 0, 0), 3600000);
+    }
+}
