35fa1384f5a7f6d3cee104f9b5925570fa4047db
ame <104745335+atinyame@users.noreply.github.com>
2022-05-15 15:44:42 -0500

    8kyu: Plural

---
+++ 8kyu/shark/plural/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {}
+
+fn plural(n: f64) -> bool {
+    n != 1.0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_plural() {
+        assert_eq!(plural(0.0), true);
+        assert_eq!(plural(0.5), true);
+        assert_eq!(plural(1.0), false);
+        assert_eq!(plural(100.0), true);
+    }
+}
