e4a58b04e0b47ffdc2df00969de96b6b1f5f554f
ame <104745335+atinyame@users.noreply.github.com>
2022-05-16 13:47:32 -0500

    8kyu: Will you make it?

---
+++ 8kyu/zero_fuel/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {}
+
+fn zero_fuel(distance_to_pump: u32, mpg: u32, gallons: u32) -> bool {
+    gallons * mpg >= distance_to_pump
+}
+
+#[cfg(test)]
+mod tests {
+    use super::zero_fuel;
+
+    #[test]
+    fn sample_tests() {
+        assert_eq!(zero_fuel(50, 25, 2), true);
+        assert_eq!(zero_fuel(100, 50, 1), false);
+    }
+}
