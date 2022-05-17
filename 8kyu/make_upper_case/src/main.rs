4da1ea1589894f7377fa36cf9e383d76e04e06f0
ame <104745335+atinyame@users.noreply.github.com>
2022-05-17 15:01:48 -0500

    8kyu: MakeUpperCase

---
+++ 8kyu/make_upper_case/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {}
+
+fn make_upper_case(s: &str) -> String {
+    s.to_uppercase()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_make_upper_case() {
+        assert_eq!(make_upper_case("hello"), "HELLO");
+    }
+}
