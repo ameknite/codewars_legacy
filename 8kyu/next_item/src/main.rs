ea8bbfcc79522e246337d0184dccabd83411a450
ame <104745335+atinyame@users.noreply.github.com>
2022-05-19 13:20:56 -0500

    8kyu: What's up next?

---
+++ 8kyu/next_item/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {}
+
+fn next_item<T: PartialEq<T> + Clone>(slice: &[T], find: T) -> Option<T> {
+    slice
+        .get(slice.iter().position(|x| *x == find).unwrap() + 1)
+        .cloned()
+}
+
+// fn next_item<T: PartialEq<T> + Clone>(slice: &[T], find: T) -> Option<T> {
+//   slice.iter().skip_while(|&x| *x != find).nth(1).cloned()
+// }
+
+#[test]
+fn returns_expected() {
+    assert_eq!(next_item(&["Joe", "Bob", "Sally"], "Bob"), Some("Sally"));
+    assert_eq!(next_item(&[0, 1], 0), Some(1));
+    assert_eq!(next_item(&[0, 1], 1), None);
+    assert_eq!(
+        next_item((1..10).collect::<Vec<_>>().as_slice(), 7),
+        Some(8)
+    );
+}
