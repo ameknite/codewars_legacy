2c6a01055869283e54d589f9b851e33947ff1bad
ame <104745335+atinyame@users.noreply.github.com>
2022-05-19 13:54:47 -0500

    8kyu: Welcome!

---
+++ 8kyu/welcome/src/main.rs
@@ -0,0 +1,38 @@
+fn main() {}
+
+fn greet(language: &str) -> &str {
+    match language {
+        "english" => "Welcome",
+        "czech" => "Vitejte",
+        "danish" => "Velkomst",
+        "dutch" => "Welkom",
+        "estonian" => "Tere tulemast",
+        "finnish" => "Tervetuloa",
+        "flemish" => "Welgekomen",
+        "french" => "Bienvenue",
+        "german" => "Willkommen",
+        "irish" => "Failte",
+        "italian" => "Benvenuto",
+        "latvian" => "Gaidits",
+        "lithuanian" => "Laukiamas",
+        "polish" => "Witamy",
+        "spanish" => "Bienvenido",
+        "swedish" => "Valkommen",
+        "welsh" => "Croeso",
+        _ => "Welcome",
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_filed() {
+        assert_eq!(greet("english"), "Welcome");
+        assert_eq!(greet("dutch"), "Welkom");
+        assert_eq!(greet("IP_ADDRESS_INVALID"), "Welcome");
+        assert_eq!(greet(""), "Welcome");
+        assert_eq!(greet("swelsh"), "Welcome");
+    }
+}
