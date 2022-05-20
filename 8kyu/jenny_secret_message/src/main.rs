83098c3f98f6bef472d51318869f571a12274af7
ame <104745335+atinyame@users.noreply.github.com>
2022-05-19 19:53:29 -0500

    8kyu: Jenny's secret message

---
+++ 8kyu/jenny_secret_message/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {}
+
+fn greet(input: &str) -> String {
+    if input == "Johnny" {
+        return "Hello, my love!".to_string();
+    };
+    format!("Hello, {}!", input)
+}
+
+#[test]
+fn greets_some_people_normally() {
+    assert_eq!(greet("Jim"), "Hello, Jim!");
+    assert_eq!(greet("Jane"), "Hello, Jane!");
+    assert_eq!(greet("Simon"), "Hello, Simon!");
+}
+
+#[test]
+fn greets_johnny_special() {
+    assert_eq!(greet("Johnny"), "Hello, my love!");
+}
