96be7ffab16ccea2daf826bfade1352cbded480f
ame <104745335+atinyame@users.noreply.github.com>
2022-05-19 19:10:18 -0500

    8kyu: Sort and Star

---
+++ 8kyu/two_sort/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {}
+
+fn two_sort(arr: &[&str]) -> String {
+    arr.iter()
+        .min()
+        .unwrap()
+        .chars()
+        .map(|x| x.to_string())
+        .collect::<Vec<_>>()
+        .join("***")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample_test_cases() {
+        assert_eq!(
+            two_sort(&[
+                "bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"
+            ]),
+            "b***i***t***c***o***i***n"
+        );
+        assert_eq!(
+            two_sort(&[
+                "turns", "out", "random", "test", "cases", "are", "easier", "than", "writing",
+                "out", "basic", "ones"
+            ]),
+            "a***r***e"
+        );
+    }
+}
