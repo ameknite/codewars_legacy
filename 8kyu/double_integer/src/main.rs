489bcfd472508a4e654f28961282855ec234d6c1
ame <104745335+atinyame@users.noreply.github.com>
2022-05-21 12:47:49 -0500

    8kyu: You Can't Code Under Pressure #1

---
+++ 8kyu/double_integer/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {}
+
+fn double_integer(n: i32) -> i32 {
+    n * 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::double_integer;
+
+    #[test]
+    fn sample_tests() {
+        assert_eq!(double_integer(1), 2);
+        assert_eq!(double_integer(5), 10);
+        assert_eq!(double_integer(10), 20);
+        assert_eq!(double_integer(20), 40);
+        assert_eq!(double_integer(40), 80);
+    }
+}
