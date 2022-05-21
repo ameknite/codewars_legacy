256f7a3427c6c00ba79af2c109b7943ff1b2be68
ame <104745335+atinyame@users.noreply.github.com>
2022-05-21 12:45:29 -0500

    8kyu: Get Nth Even Number

---
+++ 8kyu/nth_even/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {}
+
+fn nth_even(n: u32) -> u32 {
+    n * 2 - 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_tests() {
+        assert_eq!(nth_even(1), 0);
+        assert_eq!(nth_even(2), 2);
+        assert_eq!(nth_even(3), 4);
+        assert_eq!(nth_even(100), 198);
+        assert_eq!(nth_even(1298734), 2597466);
+    }
+}
