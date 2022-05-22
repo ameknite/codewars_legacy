c9ff972919e2af8b6b063ebd1359335a556a06e3
ame <104745335+atinyame@users.noreply.github.com>
2022-05-21 22:42:15 -0500

    8kyu: Grasshoper - Check for factor

---
+++ 8kyu/check_for_factor/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {}
+
+fn check_for_factor(base: i32, factor: i32) -> bool {
+    base % factor == 0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic_tests() {
+        assert_eq!(check_for_factor(10, 2), true);
+        assert_eq!(check_for_factor(63, 7), true);
+        assert_eq!(check_for_factor(2450, 5), true);
+        assert_eq!(check_for_factor(24612, 3), true);
+        assert_eq!(check_for_factor(9, 2), false);
+        assert_eq!(check_for_factor(653, 7), false);
+        assert_eq!(check_for_factor(2453, 5), false);
+        assert_eq!(check_for_factor(24617, 3), false);
+    }
+}
