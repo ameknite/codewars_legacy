23995a65babf527bc7a9a8c583ffe6051eb34b93
ame <104745335+atinyame@users.noreply.github.com>
2022-05-21 23:15:29 -0500

    8kyu: Safen User Input Part I - htmlspecialchars

---
+++ 8kyu/html_special_chars/src/main.rs
@@ -0,0 +1,43 @@
+fn main() {}
+
+fn html_special_chars(html: &str) -> String {
+    html.replace('&', "&amp;")
+        .replace('<', "&lt;")
+        .replace('>', "&gt;")
+        .replace('"', "&quot;")
+}
+
+// fn html_special_chars(html: &str) -> String {
+//     html.split("").map(|c| match c {
+//         "<" => "&lt;",
+//         ">" => "&gt;",
+//         "\"" => "&quot;",
+//         "&" => "&amp;",
+//         _ => c
+//     }).collect()
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::html_special_chars;
+
+    #[test]
+    fn sample_tests() {
+        assert_eq!(
+            html_special_chars("<h2>Hello World</h2>"),
+            "&lt;h2&gt;Hello World&lt;/h2&gt;"
+        );
+        assert_eq!(
+            html_special_chars("Hello, how would you & I fare?"),
+            "Hello, how would you &amp; I fare?"
+        );
+        assert_eq!(
+            html_special_chars("How was \"The Matrix\"?  Did you like it?"),
+            "How was &quot;The Matrix&quot;?  Did you like it?"
+        );
+        assert_eq!(
+            html_special_chars("<script>alert('Website Hacked!');</script>"),
+            "&lt;script&gt;alert('Website Hacked!');&lt;/script&gt;"
+        );
+    }
+}
