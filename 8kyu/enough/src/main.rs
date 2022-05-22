9aa32161df661fcbecd2dbe21c85b1e107d2317e
ame <104745335+atinyame@users.noreply.github.com>
2022-05-21 22:53:47 -0500

    8kyu: Will there be enough space?

---
+++ 8kyu/enough/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {}
+
+fn enough(cap: i32, on: i32, wait: i32) -> i32 {
+    if cap - on < wait {
+        return (cap - on - wait).abs();
+    }
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_enough() {
+        assert_eq!(enough(10, 5, 5), 0, "enough(10, 5, 5) should return 0");
+        assert_eq!(
+            enough(100, 60, 50),
+            10,
+            "enough(100, 60, 50) should return 10"
+        );
+        assert_eq!(enough(20, 5, 5), 0, "enough(20, 5, 5) should return 0");
+    }
+}
