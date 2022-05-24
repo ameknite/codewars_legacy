d6ff43d22e006a524e4778fff37178bd134c5331
ame <104745335+atinyame@users.noreply.github.com>
2022-05-24 15:46:14 -0500

    7kyu: Two to One

---
+++ 7kyu/longest/src/main.rs
@@ -0,0 +1,29 @@
+fn main() {}
+
+use itertools::Itertools;
+
+fn longest(a1: &str, a2: &str) -> String {
+    a1.chars().chain(a2.chars()).sorted().dedup().collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn testing(s1: &str, s2: &str, exp: &str) -> () {
+        println!("s1:{:?} s2:{:?}", s1, s2);
+        println!("{:?} {:?}", longest(s1, s2), exp);
+        println!("{}", longest(s1, s2) == exp);
+        assert_eq!(&longest(s1, s2), exp)
+    }
+
+    #[test]
+    fn basic_tests() {
+        testing("aretheyhere", "yestheyarehere", "aehrsty");
+        testing(
+            "loopingisfunbutdangerous",
+            "lessdangerousthancoding",
+            "abcdefghilnoprstu",
+        );
+    }
+}
