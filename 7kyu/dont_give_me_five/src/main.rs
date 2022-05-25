9c1efe74fb2414f436d71b90740a80931e7099dd
ame <104745335+atinyame@users.noreply.github.com>
2022-05-25 00:43:53 -0500

    7kyu: Don't give me five!

--- 7kyu/dont_give_me_five/src/main.rs
+++ 7kyu/dont_give_me_five/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+fn main() {}
+
+fn dont_give_me_five(start: isize, end: isize) -> isize {
+    (start..=end)
+        .filter(|x| !x.to_string().contains('5'))
+        .count() as isize
+}
+
+#[test]
+fn returns_expected() {
+    assert_eq!(dont_give_me_five(1, 9), 8);
+    assert_eq!(dont_give_me_five(4, 17), 12);
 }
