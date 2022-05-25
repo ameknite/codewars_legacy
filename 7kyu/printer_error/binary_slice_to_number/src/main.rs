204f8eece27dcfb345f312cadb76890162e04198
ame <104745335+atinyame@users.noreply.github.com>
2022-05-24 21:14:38 -0500

    7kyu: Ones and Zeros

---
+++ 7kyu/printer_error/binary_slice_to_number/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {}
+
+fn binary_slice_to_number(slice: &[u32]) -> u32 {
+    slice.iter().fold(0, |acc, x| acc << 1 | x)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::binary_slice_to_number;
+
+    #[test]
+    fn example_tests() {
+        assert_eq!(binary_slice_to_number(&vec![0, 0, 0, 1]), 1);
+        assert_eq!(binary_slice_to_number(&vec![0, 0, 1, 0]), 2);
+        assert_eq!(binary_slice_to_number(&vec![1, 1, 1, 1]), 15);
+        assert_eq!(binary_slice_to_number(&vec![0, 1, 1, 0]), 6);
+    }
+}
