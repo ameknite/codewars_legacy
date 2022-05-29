6bd852c8b5313017e96abcdf983d81de522647a2
ame <104745335+atinyame@users.noreply.github.com>
2022-05-29 11:53:32 -0500

    7kyu: Power of two

---
+++ 7kyu/power_of_two/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+}
+
+fn power_of_two(x: u64) -> bool {
+    x.is_power_of_two()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic_tests() {
+        assert_eq!(power_of_two(0), false);
+        assert_eq!(power_of_two(2), true);
+        assert_eq!(power_of_two(5), false);
+        assert_eq!(power_of_two(6), false);
+        assert_eq!(power_of_two(8), true);
+        assert_eq!(power_of_two(1024), true);
+        assert_eq!(power_of_two(4096), true);
+        assert_eq!(power_of_two(8191), false);
+    }
+}
