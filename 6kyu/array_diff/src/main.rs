a4ddc584037182af86f27831131bbc5fa67ff884
ame <104745335+atinyame@users.noreply.github.com>
2022-06-02 18:12:19 -0500

    6kyu: Array.diff

---
+++ 6kyu/array_diff/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {}
+
+fn array_diff<T: PartialEq>(a: Vec<T>, b: Vec<T>) -> Vec<T> {
+    a.into_iter().filter(|x| !b.contains(x)).collect()
+}
+
+// fn array_diff<T: PartialEq>(mut a: Vec<T>, b: Vec<T>) -> Vec<T> {
+//     a.retain(|x| !b.contains(x));
+//     a
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn returns_expected() {
+        assert_eq!(array_diff(vec![1, 2], vec![1]), vec![2]);
+        assert_eq!(array_diff(vec![1, 2, 2], vec![1]), vec![2, 2]);
+        assert_eq!(array_diff(vec![1, 2, 2], vec![2]), vec![1]);
+        assert_eq!(array_diff(vec![1, 2, 2], vec![]), vec![1, 2, 2]);
+        assert_eq!(array_diff(vec![], vec![1, 2]), vec![]);
+        assert_eq!(array_diff(vec![1, 2, 3], vec![1, 2]), vec![3]);
+    }
+}
