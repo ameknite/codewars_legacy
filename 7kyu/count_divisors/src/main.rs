469c383dc0ebaa61b455ba8092e5a0b8146aaa5e
ame <104745335+atinyame@users.noreply.github.com>
2022-06-02 23:24:38 -0500

    7kyu: Count the divisors of a number

---
+++ 7kyu/count_divisors/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+}
+
+
+fn divisors(n: u32) -> u32 {
+    (1..=n).filter(|x| n % x == 0).count() as u32
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::divisors;
+
+    #[test]
+    fn sample_tests() {
+        assert_eq!(divisors(1), 1);
+        assert_eq!(divisors(4), 3);
+        assert_eq!(divisors(5), 2);
+        assert_eq!(divisors(12), 6);
+        assert_eq!(divisors(25), 3);
+        assert_eq!(divisors(4096), 13);
+    }
+}
