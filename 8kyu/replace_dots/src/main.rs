aa2fdfee24548ec29e8fa2ac1272ce66e7d000ef
ame <104745335+atinyame@users.noreply.github.com>
2022-06-09 13:30:06 -0500

    8kyu: FIXME: Replace all dots

---
+++ 8kyu/replace_dots/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {}
+
+use regex::Regex;
+
+fn replace_dots(s: &str) -> String {
+    Regex::new(r"\.").unwrap().replace_all(s, "-").into_owned()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::replace_dots;
+
+    #[test]
+    fn sample_tests() {
+        assert_eq!(replace_dots(""), "");
+        assert_eq!(replace_dots("no dots"), "no dots");
+        assert_eq!(replace_dots("one.two.three"), "one-two-three");
+        assert_eq!(replace_dots("........"), "--------");
+    }
+}
