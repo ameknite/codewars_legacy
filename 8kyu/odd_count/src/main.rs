11f042ba06cad4b63abc26e07ce5f768e1ef8683
ame <104745335+atinyame@users.noreply.github.com>
2022-06-17 19:47:52 -0500

    8kyu: Count Odd Numbers below n

---
+++ 8kyu/odd_count/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {}
+
+fn odd_count(n: u64) -> u64 {
+    n / 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::odd_count;
+
+    #[test]
+    fn sample_tests() {
+        assert_eq!(odd_count(15), 7);
+        assert_eq!(odd_count(15023), 7511);
+    }
+}
