ea3ffb680bbec5c516a653345fa113841e79ff40
ame <104745335+atinyame@users.noreply.github.com>
2022-06-17 20:10:12 -0500

    8kyu: Fake Binary

---
+++ 8kyu/fake_bin/src/main.rs
@@ -0,0 +1,27 @@
+fn main() {}
+
+fn fake_bin(s: &str) -> String {
+    s.chars()
+        .map(|c| match c {
+            '0'..='4' => '0',
+            _ => '1',
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::fake_bin;
+
+    #[test]
+    fn basic_tests() {
+        assert_eq!(fake_bin("45385593107843568"), "01011110001100111");
+        assert_eq!(fake_bin("509321967506747"), "101000111101101");
+        assert_eq!(
+            fake_bin("366058562030849490134388085"),
+            "011011110000101010000011011"
+        );
+        assert_eq!(fake_bin("15889923"), "01111100");
+        assert_eq!(fake_bin("800857237867"), "100111001111");
+    }
+}
