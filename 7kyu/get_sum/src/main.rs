80ac03ddd1c252dec98e2b3069a080592158ddf9
atinyame <104745335+atinyame@users.noreply.github.com>
2022-07-01 18:47:48 -0500

    7kyu: Beginner Series #3 Sum of Numbers

---
+++ 7kyu/get_sum/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {}
+
+fn get_sum(a: i64, b: i64) -> i64 {
+    (a.min(b)..=a.max(b)).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::get_sum;
+
+    #[test]
+    fn sample_tests() {
+        assert_eq!(get_sum(0, 1), 1);
+        assert_eq!(get_sum(1, 2), 3);
+        assert_eq!(get_sum(5, -1), 14);
+        assert_eq!(get_sum(505, 4), 127759);
+    }
+}
