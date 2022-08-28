eafb27e05023f3cb2ce8921a4eeb1e7a6ebe50d1
atinyame <104745335+atinyame@users.noreply.github.com>
2022-08-27 22:29:06 -0500

    c# - 8kyu: StringRepeat

---
+++ c#/8kyu/RepeatStr/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Solution
+{
+    public static class Program
+    {
+        public static string RepeatStr(int n, string s)
+        {
+            return string.Concat(Enumerable.Repeat(s, n));
+        }
+    }
+}
