d80dee138b8cd8339d09607f763efac004ffa2d5
atinyame <104745335+atinyame@users.noreply.github.com>
2022-08-27 21:59:52 -0500

    Reversed Strings

---
+++ c#/8kyu/ReversedStrings.cs
@@ -0,0 +1,11 @@
+using System;
+
+public static class Kata
+{
+    public static string Solution(string str)
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
